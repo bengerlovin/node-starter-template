d03b19a0a8675fa0f2f801803e22bf3a1f0bf584
Ben Gerlovin <ben.gerlovin@gmail.com>
2022-03-17 19:22:13 -0400

    basic server set up

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 /**
  * Entry point file for Node.js starter
  */
+
+const express = require('express')
+const app = express();
+
+
+// Start server
+
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000...")
+})
